f1dbbf3781ce60794d62675f7e76589861e9cd31
pvaranchi001 <padmanabha.varanchi@pwc.com>
2017-11-27 20:25:20 +0530

    chekin in last few files

---
+++ storage/framework/views/1db6ab50fa2676dbb65ea9f24897dd00581670d7.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   
+<link href="<?php echo e(asset('css/main.css')); ?>" rel="stylesheet"></head>
+
+<body >
+    <!-- Root Element -->
+    <div id="app" ></div>
+<script src="<?php echo e(asset('js/bundle.js')); ?>"></script>
+</html>
