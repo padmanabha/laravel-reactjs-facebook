ceec9bb497b66ddbd92f0b67469cfcb5aebea7a8
pvaranchi001 <padmanabha.varanchi@pwc.com>
2017-11-27 19:45:55 +0530

    cleaning up git issues

--- public/src/app/common/LoginLayout.js
+++
@@ -1,9 +0,0 @@
-import React,{Component} from 'react';
-
-class LoginLayout extends Component{
-   render(){
-       return(<div>Login</div>);
-   }
-}
-
-export default LoginLayout;
--- public/src/app/redux/reducers.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux';
-
-import user from './userReducer';
-import posts from '../components/posts/redux/postReducers';
-
-export default combineReducers({
-     user,
-     posts
-});
